628a7689b72c6e877cf964f3c5f24bc5f0fe7920
Kevin Cao <kcao1998@gmail.com>
2021-04-05 00:57:34 -0400

    completed implementation section in design document for AST conversion; added comments to parser; implemented debruijn notation conversion (no tests yet)

---
+++ debruijn.rkt
@@ -0,0 +1,49 @@
+#lang racket
+(require "lib/structs.rkt")
+(provide debruijnize)
+
+; debruijnize : → AST (Listof Symbol) AST
+; debruijnize takes an AST, an environment to store variable bindings, and outputs
+; the debruijnized form of the AST
+(define (debruijnize ast env)
+  (match ast
+    [(Int n) ast]
+    [(Bool b) ast]
+    [(Str s) ast]
+    [(Empty) ast]
+    [(Op o) ast]
+    [(Var v)
+     (define idx (index-of env v))
+     (if idx
+         (Var (format "v~a" idx))
+         ast)]
+    [(If con consq alter)
+     (define con-d (debruijnize con env))
+     (define consq-d (debruijnize consq env))
+     (define alter-d (debruijnize alter env))
+     (If con-d consq-d alter-d)]
+    [(Let var val body)
+     (define val-d (debruijnize val))
+     (define new-env (cons (Var-v var) env))
+     (define body-d (debruijnize body new-env))
+     (Let var val-d body-d)]
+    [(Lambda p-ls body)
+     (define new-env (append
+                      (reverse (map Var-v p-ls))
+                      env))
+     (define body-d (debruijnize body new-env))
+     (Lambda p-ls body-d)]
+    [(Apply fn args)
+     (define fn-d (debruijnize fn env))
+     (define args-d (map debruijnize args))
+     (Apply fn-d args-d)]
+    [(Def fn-name p-ls body)
+     (define new-env (append
+                      (reverse (map Var-v p-ls))
+                      env))
+     (define body-d (debruijnize body new-env))
+     (Def fn-name p-ls body-d)]
+    [(Program defs exps)
+     (define defs-d (map debruijnize defs))
+     (define exps-d (map debruijnize exps))
+     (Program defs-d exps-d)]))
--- tests/parser_tests.rkt
+++ tests/parser_tests.rkt
@@ -18,13 +18,13 @@
 (define atom-test7
   'cons)
 
-(check-equal? (ast-exp atom-test1) (Int 4))
-(check-equal? (ast-exp atom-test2) (Bool #t))
-(check-equal? (ast-exp atom-test3) (Str "hello world"))
-(check-equal? (ast-exp atom-test4) (Empty))
-(check-equal? (ast-exp atom-test5) (Var 'myvar))
-(check-equal? (ast-exp atom-test6) (Op '+))
-(check-equal? (ast-exp atom-test7) (Op 'cons))
+(check-equal? (ast-sexp atom-test1) (Int 4))
+(check-equal? (ast-sexp atom-test2) (Bool #t))
+(check-equal? (ast-sexp atom-test3) (Str "hello world"))
+(check-equal? (ast-sexp atom-test4) (Empty))
+(check-equal? (ast-sexp atom-test5) (Var 'myvar))
+(check-equal? (ast-sexp atom-test6) (Op '+))
+(check-equal? (ast-sexp atom-test7) (Op 'cons))
 
 (define simple-test1
   '(+ 5 1))
@@ -49,17 +49,17 @@
 (define simple-test11
   '(define (func in) in))
 
-(check-equal? (ast-exp simple-test1) (Apply (Op '+) (list (Int 5) (Int 1))))
-(check-equal? (ast-exp simple-test2) (Apply (Op 'or) (list (Bool #f) (Bool #t))))
-(check-equal? (ast-exp simple-test3) (Apply (Op 'and) (list (Bool #f) (Int 25))))
-(check-equal? (ast-exp simple-test4) (Apply (Op 'cons) (list (Int 2) (Empty))))
-(check-equal? (ast-exp simple-test5) (Apply (Op 'eq?) (list (Str "hello world") (Str "hi"))))
-(check-equal? (ast-exp simple-test6) (If (Bool #f) (Int 2) (Int 5)))
-(check-equal? (ast-exp simple-test7) (If (Bool #t) (Empty) (Int 12)))
-(check-equal? (ast-exp simple-test8) (Let (Var 'x) (Int 22) (Var 'x)))
-(check-equal? (ast-exp simple-test9) (Lambda (list (Var 'x)) (Var 'x)))
-(check-equal? (ast-exp simple-test10) (Lambda (list (Var 'var)) (Var 'var)))
-(check-equal? (ast-exp simple-test11) (Def (Var 'func) (list (Var 'in)) (Var 'in)))
+(check-equal? (ast-sexp simple-test1) (Apply (Op '+) (list (Int 5) (Int 1))))
+(check-equal? (ast-sexp simple-test2) (Apply (Op 'or) (list (Bool #f) (Bool #t))))
+(check-equal? (ast-sexp simple-test3) (Apply (Op 'and) (list (Bool #f) (Int 25))))
+(check-equal? (ast-sexp simple-test4) (Apply (Op 'cons) (list (Int 2) (Empty))))
+(check-equal? (ast-sexp simple-test5) (Apply (Op 'eq?) (list (Str "hello world") (Str "hi"))))
+(check-equal? (ast-sexp simple-test6) (If (Bool #f) (Int 2) (Int 5)))
+(check-equal? (ast-sexp simple-test7) (If (Bool #t) (Empty) (Int 12)))
+(check-equal? (ast-sexp simple-test8) (Let (Var 'x) (Int 22) (Var 'x)))
+(check-equal? (ast-sexp simple-test9) (Lambda (list (Var 'x)) (Var 'x)))
+(check-equal? (ast-sexp simple-test10) (Lambda (list (Var 'var)) (Var 'var)))
+(check-equal? (ast-sexp simple-test11) (Def (Var 'func) (list (Var 'in)) (Var 'in)))
 
 (define complex-test1
   '(+ (+ 2 3) (- 5 7)))
@@ -81,14 +81,14 @@
       0
       (+ (car ls) (sum (cdr ls))))))
 
-(check-equal? (ast-exp complex-test1) (Apply (Op '+) (list (Apply (Op '+) (list (Int 2) (Int 3))) (Apply (Op '-) (list (Int 5) (Int 7))))))
-(check-equal? (ast-exp complex-test2) (Apply
+(check-equal? (ast-sexp complex-test1) (Apply (Op '+) (list (Apply (Op '+) (list (Int 2) (Int 3))) (Apply (Op '-) (list (Int 5) (Int 7))))))
+(check-equal? (ast-sexp complex-test2) (Apply
                                        (Op 'or)
                                        (list (If (Apply (Op '<) (list (Int 5) (Int 10))) (Apply (Op 'eq?) (list (Int 2) (Int 3))) (Apply (Op '>) (list (Int 5) (Int 1))))
                                              (Apply (Op 'eq?) (list (Int 2) (Int 2))))))
-(check-equal? (ast-exp complex-test3) (Let (Var 'x) (Lambda (list (Var 'a))
+(check-equal? (ast-sexp complex-test3) (Let (Var 'x) (Lambda (list (Var 'a))
                                                             (Apply (Op '+) (list (Int 1) (Var 'a)))) (Apply (Var 'x) (list (Apply (Op '+) (list (Int 3) (Int 4)))))))
-(check-equal? (ast-exp complex-test4)(Let
+(check-equal? (ast-sexp complex-test4)(Let
                                       (Var 'a)
                                       (Int 3)
                                       (Let
@@ -99,7 +99,7 @@
                                         (list
                                          (Apply (Lambda (list (Var 'x)) (Apply (Op '*) (list (Var 'x) (Var 'x)))) (list (Var 'a)))
                                          (Apply (Lambda (list (Var 'y)) (Apply (Op '*) (list (Var 'y) (Var 'y)))) (list (Var 'b))))))))
-(check-equal? (ast-exp complex-test5) (Def
+(check-equal? (ast-sexp complex-test5) (Def
                                        (Var 'sum)
                                        (list (Var 'ls))
                                        (If (Apply (Op 'eq?) (list (Empty) (Var 'ls)))
